76973411a76c19d0c7e6fc0b7d26cae7e5fc6ebe
Imad Saleem <105483840+ImadSlm@users.noreply.github.com>
2025-01-31 17:56:40 +0100

    chore: update CI workflow to include MariaDB service and adjust database configuration

--- config/database.js
+++ config/database.js
@@ -1,14 +1,14 @@
 const { Sequelize } = require("sequelize")
 require("dotenv").config()
-const database = process.env.DB_DATABASE
-const username = process.env.DB_USERNAME
-const password = process.env.DB_PASSWORD
-const host = process.env.DB_HOST
+const database = process.env.DB_APP_DATABASE
+const username = process.env.DB_APP_USERNAME
+const password = process.env.DB_APP_PASSWORD
+const host = process.env.DB_APP_HOST
 
 // Création d'une instance de Sequelize pour se connecter à la base de données
 const sequelize = new Sequelize(database, username, password, {
     host: host,
-    port: 3341,
+    port: 33,
     dialect: "mariadb",
 })
 
