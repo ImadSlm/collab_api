8a9c9850557c4212b607499cc0f177ebfc3650ec
Imad Saleem <imad.saleem@hotmail.fr>
2025-02-11 13:10:14 +0100

    feat: integrate Jira API credentials into CI workflow and configuration

--- config/jira.js
+++ config/jira.js
@@ -2,10 +2,10 @@ const axios = require("axios");
 require("dotenv").config();
 
 const jiraApi = axios.create({
-    baseURL: secrets.JIRA_BASE_URL,
+    baseURL: process.env.JIRA_BASE_URL,
     auth: {
-        username: secrets.JIRA_USERNAME,
-        password: secrets.JIRA_API_TOKEN,
+        username: process.env.JIRA_USERNAME,
+        password: process.env.JIRA_API_TOKEN,
     },
     headers: {
         "Content-Type": "application/json",
